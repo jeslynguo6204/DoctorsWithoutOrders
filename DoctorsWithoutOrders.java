4c598e3c58732825dbba3498ce06a1ec64ebe455
Jeslyn Guo <jguo@greenwichacademy.org>
2022-04-02 11:45:47 -0400

    passes first test case

--- DoctorsWithoutOrders.java
+++ DoctorsWithoutOrders.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-
 public class DoctorsWithoutOrders{
 
   public static void main(String[] args) {
     test1();
+    test2();
   }
 
   /**
@@ -21,12 +21,45 @@ public class DoctorsWithoutOrders{
                             HashMap<String, Integer> patients,
                             HashMap<String, HashSet<String>> schedule) {
 
-    //base case - the map of patients is empty, returns True
-    if (patients.isEmpty()){
+    //base case - the map of patients is empty
+    //patients is empty and doctors is not
+    if (patients.isEmpty() && !doctors.isEmpty()){
+      //System.out.println("BASE CASE TRUE");
       return true;
     }
+    //more patient hours than doctor hours
+    else if (doctors.isEmpty() && !patients.isEmpty()){
+      //System.out.println("BASE CASE FALSE");
+      return false;
+    }
 
-    //if the patient has more hours than doctors - eliminate doctor
+    //copy the map of doctors and patients to be passed through again
+    HashMap<String, Integer> newDoctors = new HashMap<>();
+    newDoctors.putAll(doctors);
+    HashMap<String, Integer> newPatients = new HashMap<>();
+    newPatients.putAll(patients);
+    //System.out.println(newDoctors.keySet().toString());
+    //System.out.println(newPatients.keySet().toString() + "\n");
+
+    //loops through the patients to determine if there is a doctor that can handle each
+    for (String currPatient: newPatients.keySet()){
+      Integer currPatientHours = newPatients.get(currPatient);
+      //System.out.println(currPatient + ": " + currPatientHours);
+      //if the patient has more hours than doctors - eliminate doctor as option for the patient
+      String currDoctor = newDoctors.keySet().iterator().next();
+      Integer currDoctorHours = newDoctors.get(currDoctor);
+        //Integer currDoctorHours = newDoctors.get(currDoctor);
+        //System.out.println(currDoctor + ": " + currDoctorHours);
+        //patient cannot be seen by current doctor
+        if (currPatientHours > currDoctorHours){
+          newDoctors.remove(currDoctor);
+          canAllPatientsBeSeen(newDoctors, newPatients, schedule);
+        }
+        //if the patient can be seen by the current doctor, subtract patient hours from doctor
+        currDoctorHours = currDoctorHours - currPatientHours;
+        newPatients.remove(currPatient);
+        canAllPatientsBeSeen(newDoctors, newPatients, schedule);
+    }
     //else, subtract patient hours from doctor and update fake copy of DoctorsWithoutOrders
     //canSeeAll()
     //do want to edit the schedule parameter
@@ -42,12 +75,72 @@ public class DoctorsWithoutOrders{
     return false;
   }
 
-  public static void testCustom() {
-    // ~ Your code goes here! ~
+  public static void testCustom1() {
+    System.out.println("Patient needs zero hours while doctor has some given number of hours.");
+    HashMap<String, HashSet<String>> schedule = new HashMap<>();
+
+    HashMap<String, Integer> doctors = new HashMap<>() {{
+        put("Dr. D", 8);
+    }};
+
+    HashMap<String, Integer> patients = new HashMap<String, Integer>() {{
+        put("Lucky Person", 0);  //does not need any treatment
+    }};
+
+    System.out.println(true == canAllPatientsBeSeen(doctors, patients, schedule));
+    System.out.println();
+  }
+
+  public static void testCustom2(){
+    System.out.println("Two doctors and one patient, all with the same number of hours.");
+    HashMap<String, HashSet<String>> schedule = new HashMap<>();
+
+    HashMap<String, Integer> doctors = new HashMap<>() {{
+        put("Dr. D", 8);
+        put("Dr. E", 8);
+    }};
+
+    HashMap<String, Integer> patients = new HashMap<String, Integer>() {{
+        put("Patient A", 8);  //has same number of hours as both doctors
+    }};
+
+    System.out.println(true == canAllPatientsBeSeen(doctors, patients, schedule));
+    System.out.println();
+  }
+
+  public static void testCustom3(){
+    System.out.println("Doctor has zero hours while patient needs some given number of hours.");
+    HashMap<String, HashSet<String>> schedule = new HashMap<>();
+
+    HashMap<String, Integer> doctors = new HashMap<>() {{
+        put("Dr. D", 0);
+    }};
+
+    HashMap<String, Integer> patients = new HashMap<String, Integer>() {{
+        put("Unlucky Patient", 8);  //cannot be treated
+    }};
+
+    System.out.println(false == canAllPatientsBeSeen(doctors, patients, schedule));
+    System.out.println();
+  }
+
+  public static void testCustom4(){
+    System.out.println("Doctor and patient both have zero hours.");
+    HashMap<String, HashSet<String>> schedule = new HashMap<>();
+
+    HashMap<String, Integer> doctors = new HashMap<>() {{
+        put("Dr. D", 0);
+    }};
+
+    HashMap<String, Integer> patients = new HashMap<String, Integer>() {{
+        put("Patient A", 0);  //does not require treatment
+    }};
+
+    System.out.println(true == canAllPatientsBeSeen(doctors, patients, schedule));
+    System.out.println();
   }
 
   // Provided test cases below.
-
   public static void test1() {
     System.out.println("Provided: Can't schedule if a patient requires more hours than any doctor has.");
     HashMap<String, HashSet<String>> schedule = new HashMap<>();
